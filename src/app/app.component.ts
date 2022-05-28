b1338d4f5a4456330a176b139f0b0d1af0562b1e
Suraj Saurabh <suraj@liftoffllc.com>
2022-05-29 04:06:53 +0530

    latest changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { NgForm } from '@angular/forms';
+// import { FormGroup, FormArray, FormControlName } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'TwitterApi';
+  @ViewChild('f') form: NgForm;
+  tempTweets: Array<any> = [];
+  tweets: Array<any> = [];
+  searchParams: string = "";
+  loading: boolean = false;
+  isError = false;
+  isData = false;
+  params: string = "";
+  errorMessage = '';
+
+  constructor(private http : HttpClient) {
+
+  }
+
+  ngOnInit() { }
+
+  onSubmit() {
+    this.tweets = [];
+    this.loading = true;
+    this.params=this.searchParams;
+    this.http.get<any>("http://localhost:3000/api/tweet/search/"+ this.searchParams).subscribe(
+      result => {
+        this.tempTweets = result.data;
+      }, errorMessage => {
+        this.isError = true; 
+        this.errorMessage = errorMessage.message;
+      } 
+    ).add(() => {
+      this.tempTweets.forEach(tweet => {
+        this.http.get<any>("http://localhost:3000/api/tweet/" + tweet.id).subscribe(
+          result => {
+            this.isData = true;
+            this.tweets.push(result);
+            console.log(result);
+          }
+          
+        )
+      });
+      this.loading = false;
+    });
+    this.form.reset();
+  }
+
+  transform(value: any) {
+    if (value?.length > 18) {
+        return value.substr(0, 18) + ' ...';
+    } else {
+        return value;
+    }   
+}
 }

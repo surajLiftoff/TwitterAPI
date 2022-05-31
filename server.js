2506a91cace64e6a3efad4fd2436d068011b1ed9
Suraj Saurabh <suraj@liftoffllc.com>
2022-05-31 11:15:57 +0530

    updated server file

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<twitter-api>'));
+app.use(express.static(__dirname + '/dist/twitter-api'));
 
 app.get('/*', function(req, res) {
 
-    res.sendFile(path.join(__dirname + '/dist/<twitter-api>/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/twitter-api/index.html'));
 });
 
 // Start the app by listening on the default Heroku port

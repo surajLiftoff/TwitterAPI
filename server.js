923cb5699e90d884a11fc109b92413da0845b040
Suraj Saurabh <suraj@liftoffllc.com>
2022-05-31 17:23:05 +0530

    working-fine-right?

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 //Install express server
 const express = require('express');
+var cors = require('cors');
 const path = require('path');
 
 const app = express();
+
+app.use(cors());
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/twitter-api'));

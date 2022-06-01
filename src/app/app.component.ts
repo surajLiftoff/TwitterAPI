24ecad2e91c7ec9021fa509c9610f966324a3fbb
Suraj Saurabh <suraj@liftoffllc.com>
2022-06-01 13:31:50 +0530

    service pakage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { NgForm } from '@angular/forms';
+import { AppService } from './app.service';
 // import { FormGroup, FormArray, FormControlName } from '@angular/forms';
 
 @Component({
@@ -19,36 +20,38 @@ export class AppComponent {
   params: string = "";
   errorMessage = '';
 
-  constructor(private http : HttpClient) {
+  constructor(private http : HttpClient,
+              private appService: AppService) {
 
   }
 
   ngOnInit() { }
 
   onSubmit() {
-    this.tweets = [];
-    this.loading = true;
-    this.params=this.searchParams;
-    this.http.get<any>("http://localhost:3000/api/tweet/search/"+ this.searchParams).subscribe(
-      result => {
-        this.tempTweets = result.data;
-      }, errorMessage => {
-        this.isError = true; 
-        this.errorMessage = errorMessage.message;
-      } 
-    ).add(() => {
-      this.tempTweets.forEach(tweet => {
-        this.http.get<any>("http://localhost:3000/api/tweet/" + tweet.id).subscribe(
-          result => {
-            this.isData = true;
-            this.tweets.push(result);
-            console.log(result);
-          }
+    // this.tweets = [];
+    // this.loading = true;
+    // this.params=this.searchParams;
+    // this.http.get<any>("http://localhost:3000/api/tweet/search/"+ this.searchParams).subscribe(
+    //   result => {
+    //     this.tempTweets = result.data;
+    //   }, errorMessage => {
+    //     this.isError = true; 
+    //     this.errorMessage = errorMessage.message;
+    //   } 
+    // ).add(() => {
+    //   this.tempTweets.forEach(tweet => {
+    //     this.http.get<any>("http://localhost:3000/api/tweet/" + tweet.id).subscribe(
+    //       result => {
+    //         this.isData = true;
+    //         this.tweets.push(result);
+    //         console.log(result);
+    //       }
           
-        )
-      });
-      this.loading = false;
-    });
+    //     )
+    //   });
+    //   this.loading = false;
+    // });
+    this.appService.submit();
     this.form.reset();
   }
 
---
+++ src/app/app.service.ts
@@ -0,0 +1,46 @@
+
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppService {
+
+  tempTweets: Array<any> = [];
+  tweets: Array<any> = [];
+  searchParams: string = "";
+  loading: boolean = false;
+  isError = false;
+  isData = false;
+  params: string = "";
+  errorMessage = '';
+
+  constructor(private http: HttpClient) { }
+
+  submit(){
+    this.tweets = [];
+    this.loading = true;
+    this.params=this.searchParams;
+    return this.http.get<any>("https://demo-heroku-express.herokuapp.com/api/tweet/search/"+ this.searchParams).subscribe(
+      result => {
+        this.tempTweets = result.data;
+      }, errorMessage => {
+        this.isError = true; 
+        this.errorMessage = errorMessage.message;
+      } 
+    ).add(() => {
+      this.tempTweets.forEach(tweet => {
+        this.http.get<any>("https://demo-heroku-express.herokuapp.com/api/tweet/" + tweet.id).subscribe(
+          result => {
+            this.isData = true;
+            this.tweets.push(result);
+            console.log(result);
+          }
+          
+        )
+      });
+      this.loading = false;
+    });
+  }
+}
